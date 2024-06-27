146e0b412fae4cd27fd953ef47b5db86cb4e9905
Alexei <alexeibradshaw12@gmail.com>
2024-06-27 14:35:16 +0100

    Added styling to the add and remove icons

--- tm470project/components/ProductGrid.js
+++ tm470project/components/ProductGrid.js
@@ -4,15 +4,15 @@ export default function ProductGrid() {
   return (
     <div>
       <div className="grid grid-cols-4 gap-10 justify-items-center items-center">
-        <ProductItem width={150} height={150} icons="false" />
-        <ProductItem width={150} height={150} icons="false" />
-        <ProductItem width={150} height={150} icons="false" />
-        <ProductItem width={150} height={150} icons="false" />
+        <ProductItem width={150} height={150} icons="true" />
+        <ProductItem width={150} height={150} icons="true" />
+        <ProductItem width={150} height={150} icons="true" />
+        <ProductItem width={150} height={150} icons="true" />
       </div>
       <div className="flex gap-10 justify-center pt-10">
-        <ProductItem width={150} height={150} icons="false" />
-        <ProductItem width={150} height={150} icons="false" />
-        <ProductItem width={150} height={150} icons="false" />
+        <ProductItem width={150} height={150} icons="true" />
+        <ProductItem width={150} height={150} icons="true" />
+        <ProductItem width={150} height={150} icons="true" />
       </div>
       <div className="flex gap-10 justify-center pt-10">
         <ProductItem
--- tm470project/components/ProductItem.js
+++ tm470project/components/ProductItem.js
@@ -1,5 +1,6 @@
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
+import { useState } from "react";
 export default function ProductItem({
   width,
   height,
@@ -8,6 +9,10 @@ export default function ProductItem({
   flexDirectionOuter = "row",
   flexDirectionInner = "col",
 }) {
+  const [isAddIconHovered, setIsAddIconHovered] = useState(false);
+  const [isRemoveIconHovered, setIsRemoveIconHovered] = useState(false);
+  
+
   const heightWidthAdjust = fixHeightWidth === "true" ? "w-36 h-36" : "";
   const flexOuterAdjust = flexDirectionOuter === "row" ? "row" : "col";
   const flexInnerAdjust = flexDirectionInner === "row" ? "row" : "col";
@@ -22,9 +27,26 @@ export default function ProductItem({
         className="w-full sm:w-1/2 md:w-2/3 lg:w-4/4"
       />
       {icons === "true" && (
-        <div className={`flex flex-${flexInnerAdjust} justify-center w-1/3 sm:w-1/2 md:w-2/3 lg:w-4/4`}>
-          <AddIcon fontSize="large" />
-          <RemoveIcon fontSize="large" />
+        <div
+          className={`flex flex-${flexInnerAdjust} justify-center w-1/3 sm:w-1/2 md:w-2/3 lg:w-4/4`}
+        >
+          <AddIcon
+            onMouseEnter={() => setIsAddIconHovered(true)}
+            onMouseLeave={() => setIsAddIconHovered(false)}
+            style={{
+              color: isAddIconHovered ? "blue" : "black",
+              fontSize: isAddIconHovered ? "45px" : "35px", // Use inline style for fontSize
+              transition: "font-size 0.3s", // Optional: smooth transition for the size change
+            }}
+          />
+          <RemoveIcon fontSize="large" 
+          onMouseEnter={() => setIsRemoveIconHovered(true)}
+          onMouseLeave={() => setIsRemoveIconHovered(false)}
+          style={{
+            color: isRemoveIconHovered ? "blue" : "black",
+            fontSize: isRemoveIconHovered ? "45px" : "35px", // Use inline style for fontSize
+            transition: "font-size 0.3s", // Optional: smooth transition for the size change
+          }}/>
         </div>
       )}
     </div>
